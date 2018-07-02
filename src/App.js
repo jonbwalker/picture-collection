5a17e73c1fc762cca27c3576c35711b57c97af30
jon walker <jonathan.b.walker0@gmail.com>
2018-07-01 21:35:36 -0700

    fetch user profile data and display instagram profile image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: undefined,
+      profileImage: undefined
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUserData()
+      .then(this.fetchUserDataSuccess)
+  }
+
+  async fetchUserData() {
+    const instagramUserEndpoint = `https://api.instagram.com/v1/users/self?access_token=${window.location.hash.substring(1).split('=')[1]}`
+    let response = await fetch(instagramUserEndpoint);
+    return response.json();
+  }
+
+  fetchUserDataSuccess = (userData) => {
+    this.setState({userInfo: userData.data, profileImage: userData.data.profile_picture});
+  };
+
+  renderUserData() {
+    if (this.state.userInfo) {
+      return  <Fragment>
+        <img src={this.state.profileImage} alt={'User'}/>
+        <p>{this.state.userInfo.bio}</p>
+      </Fragment>
+    } else {
+     return <div>Loading...</div>
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +45,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.renderUserData()}
         </p>
       </div>
     );
